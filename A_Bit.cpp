48670ae28aef0c98e61f4851861a39429a3bc263
jui-9545 <sumaiyajahan9545@gmail.com>
2024-03-28 20:04:52 +0600

    My first website

---
+++ A_Bit.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+        int n;
+        cin>>n;
+        int x=0;
+         for(int i=0;i<n;i++){
+            string s;
+            cin>>s;
+            if(s[0]=='+'||s[1]=='+'){
+                ++x;
+            }
+            else{
+                --x;
+            }
+
+         }  
+         cout<<x<<endl; 
+}
